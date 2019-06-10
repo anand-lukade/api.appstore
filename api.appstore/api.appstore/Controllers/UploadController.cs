94ed7d0eacd9d4b585d14ffe188acce28649e4b4
Anand Lukade <arlukade@gmail.com>
2019-06-10 05:59:09 +0100

    Upload controller added

---
+++ api.appstore/api.appstore/Controllers/UploadController.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections.Generic;
+using System.Net;
+using System.Net.Http;
+using System.Web;
+using System.Web.Http;
+
+namespace api.appstore.Controllers
+{
+    public class UploadController : ApiController
+    {
+        public HttpResponseMessage Post()
+        {
+            HttpResponseMessage result = null;
+            var httpRequest = HttpContext.Current.Request;
+            if (httpRequest.Files.Count > 0)
+            {
+                var docfiles = new List<string>();
+                foreach (string file in httpRequest.Files)
+                {
+                    var postedFile = httpRequest.Files[file];
+                    var filePath = HttpContext.Current.Server.MapPath("~/UploadedFiles" + postedFile.FileName);
+                    postedFile.SaveAs(filePath);
+                    docfiles.Add(filePath);
+                }
+                result = Request.CreateResponse(HttpStatusCode.Created, docfiles);
+            }
+            else
+            {
+                result = Request.CreateResponse(HttpStatusCode.BadRequest);
+            }
+            return result;
+        }
+    }
+}
