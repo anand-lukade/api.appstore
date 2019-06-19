0b10dec322b435bb23540f1339b066a9da2802bd
Anand Lukade <arlukade@gmail.com>
2019-06-19 12:04:41 +0100

    Added logger functionality

---
+++ api.appstore/api.appstore/Models/Logger.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace api.appstore.Models
+{
+    public static class Logger
+    {
+        public static void Log(string message)
+        {
+            using (MususAppEntities entity = new MususAppEntities())
+            {
+                entity.LogMasters.Add(new LogMaster()
+                {
+                    logtext = message,
+                    logtime = DateTime.UtcNow,
+                    //LogType=logType
+                });
+            }
+        }
+    }
+}
