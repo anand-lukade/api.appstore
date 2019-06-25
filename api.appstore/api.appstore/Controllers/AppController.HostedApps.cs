b85854756f72295cbf646357d1bc1a3085f03a84
Anand Lukade <arlukade@gmail.com>
2019-06-25 21:54:37 +0530

    bug fix

--- api.appstore/api.appstore/Controllers/AppController.HostedApps.cs
+++ api.appstore/api.appstore/Controllers/AppController.HostedApps.cs
@@ -146,13 +146,13 @@ namespace api.appstore.Controllers
             CloudBlobClient blobClient = storageAccount.CreateCloudBlobClient();            
             CloudBlobContainer container = blobClient.GetContainerReference(ConfigurationManager.AppSettings["containerName"]);        
             CloudBlockBlob blockBlob = container.GetBlockBlobReference(DateTime.Now.ToString("hhmmss")+"_"+file.FileName);
-            if(flag)
-            {
-                blockBlob = container.GetBlockBlobReference("BTJG3912.JPG");
-                blockBlob.Properties.ContentType = file.ContentType;
-                blockBlob.Properties.ContentMD5 = null;
-                blockBlob.SetProperties();
-            }            
+            //if(flag)
+            //{
+            //    //blockBlob = container.GetBlockBlobReference("BTJG3912.JPG");
+            //    //blockBlob.Properties.ContentType = file.ContentType;
+            //    //blockBlob.Properties.ContentMD5 = null;
+            //    //blockBlob.SetProperties();
+            //}            
             blockBlob.UploadFromStream(file.InputStream);
             return blockBlob.Uri.AbsoluteUri.ToString();
         }
